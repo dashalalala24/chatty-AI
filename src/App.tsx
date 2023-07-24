7ea8ad3fa83a37b7e96f18fa4c853a0cd9c3b419
Olesia Erastova <olesia1205@yandex.ru>
2023-07-24 13:30:57 +0300

    добавила MediaStreamRecorder для записи голоса

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
+import record from './utils/MediaStream';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>Chatty AI</header>
+      <h1>Record Your Voice</h1>
+      <div>
+        <button className='app__record-button' type='button' onClick={record} id='record-button' />
+      </div>
     </div>
   );
 }
---
+++ src/utils/MediaStream.js
@@ -0,0 +1,47 @@
+let chunks = [];
+let mediaRecorder = null;
+let audioBlob = null;
+
+function mediaRecorderDataAvailable(evt) {
+  chunks.push(evt.data);
+}
+
+function mediaRecorderStop() {
+  // console.log("data available after MediaRecorder.stop() called.");
+  const audio = document.createElement('audio');
+
+  // audioBlob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+  audioBlob = new Blob(chunks, { type: 'audio/mp3' });
+  const audioURL = window.URL.createObjectURL(audioBlob);
+  audio.src = audioURL;
+  // console.log("recorder stopped");
+
+  mediaRecorder = null;
+  chunks = [];
+}
+
+export default async function record() {
+
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    alert('Your browser does not support recording!');
+    return;
+  }
+
+  if (!mediaRecorder) {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      })
+      mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start();
+      // console.log(mediaRecorder.state);
+      // console.log("recorder started");
+      mediaRecorder.ondataavailable = mediaRecorderDataAvailable;
+      mediaRecorder.onstop = mediaRecorderStop;
+    } catch (err) {
+      alert(`The following error occurred: ${err}`);
+    }
+  } else {
+    mediaRecorder.stop();
+  }
+};
