cbbeeeb5f2d77dfbdf461f667c409cb28a5474b2
Olesia Erastova <olesia1205@yandex.ru>
2023-07-26 20:04:28 +0300

    верстка компонента sidebar

--- src/components/Main/Sidebar/Sidebar.tsx
+++ src/components/Main/Sidebar/Sidebar.tsx
@@ -4,7 +4,14 @@ import './Sidebar.css';
 const Sidebar: FC = () => {
   return (
     <section className='sidebar'>
-
+      <div className='sidebar__nav'>
+        <h2 className='sidebar__subtitle'>История</h2>
+        <button className='sidebar__search-button'></button>
+      </div>
+      <section className='sidebar__history-container'>
+        <h3 className='sidebar__history-subtitle'>Запросов пока нет</h3>
+      </section>
+      <button className='sidebar__newchat-button'>Новый чат</button>
     </section>
   );
 }
