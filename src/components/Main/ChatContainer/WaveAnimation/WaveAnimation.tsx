a34b28e7cc6e39b4e35132a5b91cecf1f8e88941
Daria Lazarchuk <dashal_O_o@mail.ru>
2023-07-29 15:11:33 +0300

    feat: speechflowApi, fix:animation

--- src/components/Main/ChatContainer/WaveAnimation/WaveAnimation.tsx
+++ src/components/Main/ChatContainer/WaveAnimation/WaveAnimation.tsx
@@ -1,29 +1,29 @@
-import { FC, useContext } from "react";
-import "./WaveAnimation.css";
-import { CurrentUserContext } from "../../../../contexts/CurrentUserContext";
-import record from "../../../../utils/MediaStream";
-import Animation from "../../../../utils/Animation";
+import { useEffect, useRef } from "react";
 
-const WaveAnimation: FC = () => {
-  const isRecording = useContext(CurrentUserContext);
+import "./WaveAnimation.css";
+import Animation from "../../../../utils/Animation";
+import Record from "../Record/Record";
+import { render } from "react-dom";
+
+const WaveAnimation = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const containerNode = containerRef.current;
+    const wavesContainer = document.getElementById("wave__animation");
+    if (containerNode && wavesContainer) {
+      render(
+        <Animation inMin={0} inMax={1} outMin={-0.05} outMax={-0.02} />,
+        wavesContainer
+      );
+    }
+  }, []);
 
   return (
-    <div className="wave__section">
-      <div className="wave__record-block" id="wave__record-block">
-        <Animation inMin={0} inMax={100} outMin={-0.0005} outMax={-0.0002} />
-        <div className="wave__record">
-          <button
-            className="wave__record-button"
-            type="button"
-            onClick={() => {
-              isRecording ? record() : record();
-            }}
-            id="record-button"
-          />
-          <h2 className="wave__record-subtitle">
-            Помогу в работе. Только спроси
-          </h2>
-        </div>
+    <div className="wave__section" id="wave__section" ref={containerRef}>
+      <div className="wave__animation" id="wave__animation"></div>
+      <div className="wave__record" id="wave__record">
+        <Record />
       </div>
     </div>
   );
